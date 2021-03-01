32b7a942a61263ecf0faec6137923ffa8b008e7e
Tony Diana <info@tonydiana.es>
2021-03-01 17:14:44 -0600

    .

--- img/conf/lanzadores/zeusBeta.sh
+++ img/conf/lanzadores/zeusBeta.sh
@@ -4,6 +4,8 @@ cd /home/pi/zeus
 bash ./Super_pre.sh
 git add .
 git commit -m "Ok"
+git fetch --all
+git reset --hard origin/beta
 git pull origin beta
 bash ./Super_post.sh
 
--- img/conf/lanzadores/zeusMaster.sh
+++ img/conf/lanzadores/zeusMaster.sh
@@ -4,6 +4,8 @@ cd /home/pi/zeus
 bash ./Super_pre.sh
 git add .
 git commit -m "Ok"
+git fetch --all
+git reset --hard origin/master
 git pull origin master
 bash ./Super_post.sh
 
